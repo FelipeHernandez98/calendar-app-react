007ab0d30a8a96ed46d9b81d62de98efaebf10b9
unknown <andresfelipehc@ufps.edu.co>
2022-08-22 14:34:48 -0500

    agregando el react-big-calendar y poniendo estilos propios

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,9 +1,53 @@
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+import { Calendar } from 'react-big-calendar';
 import { NavBar } from '../';
+import { addHours } from 'date-fns'
+import { getMessagesES, localizer } from '../../helpers';
+
+
+const events = [{
+  title: 'Recordatorio 1',
+  notes: 'Hay que comprar ropa',
+  start: new Date(),
+  end: addHours( new Date(), 2),
+  bgColor: '#fafafa',
+  user: {
+    _id: '123',
+    name: 'Felipe'
+  }
+}]
 
 export const CalendarPage = () => {
+
+  const eventStyleGetter = ( event, start, end , isSelected ) =>{
+
+    const style = {
+      backgroundColor: '#347CF7',
+      borderRadius: '0px',
+      opacity: 0.8,
+      color: 'white'
+    }
+
+    return {
+      style
+    }
+  }
+
   return (
     <>
       <NavBar />
+
+      <Calendar
+        culture='es'
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 'calc(100vh - 80px )' }}
+        messages={ getMessagesES() }
+        eventPropGetter= { eventStyleGetter }
+      />
+
     </>
   )
 }
